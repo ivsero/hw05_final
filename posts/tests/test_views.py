8c72abc51a5af098201b60103ad8159ab1351b2d
Kartezi <pusta.lb.lb@gmail.com>
2021-03-31 22:49:30 +0300

    на ревью PEP8 3

--- posts/tests/test_views.py
+++ posts/tests/test_views.py
@@ -209,14 +209,14 @@ class PostsPagesTests(TestCase):
             reverse('add_comment', kwargs={
                     'username': self.user,
                     'post_id': post.id
-            }
-            ), data=form_fields, follow=True)
+            }), data=form_fields, follow=True
+            )
         response = self.authorized_client.get(
             reverse('post', kwargs={
                 'username': self.user,
                 'post_id': post.id
-            }
-            ), data=form_fields, follow=True)
+            }), data=form_fields, follow=True
+            )
 
         self.assertContains(response, 'тестовый комментарий')
 
