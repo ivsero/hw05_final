2e407945f1d14843c823ec99aaada3c0298a80a3
Kartezi <pusta.lb.lb@gmail.com>
2021-04-02 16:34:18 +0300

    После ревью3

--- posts/views.py
+++ posts/views.py
@@ -199,7 +199,6 @@ def profile_follow(request, username):
 
 @login_required
 def profile_unfollow(request, username):
-    user = request.user
     user_profile = get_object_or_404(USER, username=username)
     Follow.objects.filter(
         user=request.user,
