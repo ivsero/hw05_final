ef2ce28a3067643b9fa26650b85c28c7a1d21745
Kartezi <pusta.lb.lb@gmail.com>
2021-08-11 21:38:24 +0300

    gitignore

--- yatube/urls.py
+++ yatube/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
     path('auth/', include('users.urls')),
     path('auth/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
-    path('', include('posts.urls')),
+    path('', include('posts.urls')),   
 ]
 
 if settings.DEBUG:
